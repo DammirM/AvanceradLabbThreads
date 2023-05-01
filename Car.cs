5dcdb0f21b191573baf21363865965e092027fa3
Damir Mehic <damir.mehic@edu.varberg.se>
2023-05-01 22:58:52 +0200

    modified the format of time and the distance covered

---
+++ Car.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace AvanceradLabbThreads
+{
+    internal class Car
+    {
+        public string Name { get; set; }
+        public int Speed { get; set; }
+        public double Distance { get; set; }
+
+        public static string Winner;
+        public TimeSpan RaceTime { get; set; }
+
+        public Car(string name)
+        {
+            Name = name;
+            Speed = 120;
+            Distance = 0;
+        }
+
+        public void RaceStart()
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+
+            Console.WriteLine($"{Name} started the Race!!");
+
+            int i = 0;
+            while (Distance < 1 && i <= 300)
+            {
+                Thread.Sleep(1000);
+                Distance += (double)Speed / 3600;
+
+               
+
+                if (i % 5 == 0 && i != 0)
+                {
+                    CarTrouble();
+                }
+                i++;
+            }
+
+            Console.WriteLine($"{Name} has finished the race");
+
+            if (Car.Winner == null)
+            {
+                
+                Car.Winner = Name;
+                Console.WriteLine($"Contragulations to the Winner: -----  {Name}  ----");
+            }
+
+            RaceTime = sw.Elapsed;
+
+        }
+     
+        public void CarTrouble()
+        {
+            Random random = new Random();
+
+            int randomIncident = random.Next(1, 51);
+
+            if (randomIncident == 1)
+            {
+                OutofGas();
+            }
+            else if (randomIncident > 1 && randomIncident <= 3)
+            {
+                Punction();
+            }
+            else if (randomIncident > 3 && randomIncident <= 8)
+            {
+                BirdonWind();
+            }
+            else if (randomIncident > 8 && randomIncident <= 18)
+            {
+                EngineProblem();
+            }
+
+        }
+
+        public void OutofGas()
+        {
+            Console.WriteLine($"{Name} has run out of fuel and requires 3 seconds to refill the gas tank.");
+            Thread.Sleep(3000);
+        }
+
+        public void Punction()
+        {
+            Console.WriteLine($"{Name} has a flat tire it takes 2 seconds to fix that");
+            Thread.Sleep(2000);
+        }
+
+        public void BirdonWind()
+        {
+            Console.WriteLine($"{Name} has a bird on the windshield, it takes 1 seconds to fix that.");
+            Thread.Sleep(1000);
+        }
+
+        public void EngineProblem()
+        {
+            Speed = Speed - 1;
+            Console.WriteLine($"{Name} has engine problem, the speed is reduced by 1km/h, new {Speed} is km/h");
+        }
+
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,13 +1,78 @@
-﻿namespace AvanceradLabbThreads
+﻿using AvanceradLabbThreads;
+using System;
+using System.Diagnostics;
+
+namespace AvanceradLabbThreads
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static List<Car> Race = new List<Car>();
+
+        public static void Main(string[] args)
         {
 
+
+            Car car1 = new Car("Renault");
+            Car car2 = new Car("Kia");
+            Car car3 = new Car("Opel");
+
+            Race.Add(car1);
+            Race.Add(car2);
+            Race.Add(car3);
+
+            Thread thread1 = new Thread(car1.RaceStart);
+            Thread thread2 = new Thread(car2.RaceStart);
+            Thread thread3 = new Thread(car3.RaceStart);
+
+
+            Thread thread4 = new Thread(() => StatusCheck());
+
+            thread1.Start();
+            thread2.Start();
+            thread3.Start();
+
+           
+                Console.Clear();
+                Console.WriteLine("Race status:");
+                foreach (var car in Race)
+                {
+                    Console.WriteLine($"{car.Name}: {car.Distance:F2} km, {car.Speed} km/h");
+                }
+                Console.WriteLine();
             
+
+            thread4.Start();
+
+            thread1.Join();
+            thread2.Join();
+            thread3.Join();
+
+
+            Console.WriteLine("Time Scores");
+            foreach (var item in Race)
+            {
+                Console.WriteLine($"Car: {item.Name} Time: {item.RaceTime.ToString("mm\\:ss\\.fff")} Ending Speed: {item.Speed}");
+            }
         }
 
-     
+        public static void StatusCheck()
+        {
+            while (true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    string input = Console.ReadLine();
+                    if (input == "status")
+                    {
+                        foreach (var item in Race)
+                        {
+                            Console.WriteLine($"{item.Name} has driven {item.Distance.ToString("0.00")} and is at a speed of {item.Speed} km/h");
+                        }
+                    }
+                }
+                Thread.Sleep(100);
+            }
+        }
+
     }
 }
